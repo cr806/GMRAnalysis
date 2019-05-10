d0d950a5a8c79fbefd2b1f34299686c3b985c3c6
Josh Male <50483307+jm1261@users.noreply.github.com>
2019-05-10 19:18:20 +0100

    Filled in some functions
    Filled in majority of IO functions, and some data prep functions so far. Editing normalise.py to compensate for the change but will upload when more is complete

--- GMR/DataPreparation.py
+++ GMR/DataPreparation.py
@@ -1,5 +1,21 @@
-def pwr_norm():
-    pass
+import os
+import numpy as np
+
+
+def pwr_norm(dir_name):
+    '''
+    Read in and process the power spectrum data file
+    Args:
+        dir_name: <string> directory path to power spectrum
+    '''
+    power_spectrum = os.path.join(dir_name, 'power_specrum.csv')
+    step, wl, f, power = np.genfromtxt(power_spectrum,
+                                       delimiter=',',
+                                       skip_header=1,
+                                       unpack=True)
+    max_element = np.amax(power)
+    norm_power = power / max_element
+    return step, wl, f, power, norm_power
 
 
 def bg_norm():
