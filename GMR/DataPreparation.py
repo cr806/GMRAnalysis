9d9f71dd9d8cfbd88face48da3168cd22d13912b
Christopher Reardon <chris@nas10-240-38-207.york.ac.uk>
2019-05-10 17:32:12 +0100

    New package setup with empty modules and functions

---
+++ GMR/DataPreparation.py
@@ -0,0 +1,18 @@
+def pwr_norm():
+    pass
+
+
+def bg_norm():
+    pass
+
+
+def trim_spec():
+    pass
+
+
+def trim_time():
+    pass
+
+
+def roi():
+    pass
---
+++ GMR/InputOutput.py
@@ -0,0 +1,26 @@
+def exp_in():
+    pass
+
+
+def raw_in():
+    pass
+
+
+def array_in():
+    pass
+
+
+def data_array_out():
+    pass
+
+
+def png_out():
+    pass
+
+
+def csv_out():
+    pass
+
+
+def user_in():
+    pass
