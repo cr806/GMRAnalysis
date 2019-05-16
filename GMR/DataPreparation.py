1142d0e1c2bd1099625c895513e4c7ebf3a6ca53
Josh Male <50483307+jm1261@users.noreply.github.com>
2019-05-16 12:08:14 +0100

    time and filesize calibrations

--- GMR/DataPreparation.py
+++ GMR/DataPreparation.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import GMR.InputOutput as io
 
 def pwr_norm(image_data, file_name, norm_power, dir_name):
     '''
@@ -74,3 +75,80 @@ def roi():
     data for further processing.
     '''
     pass
+
+
+def normalise_file_size(number_files,
+                        image_save=False):
+    '''
+    Calculates the total file size output for normalising images and
+    processing a data cube.
+    Args:
+        number_files: <int> number of files to process
+        image_save: <bool> if images are saved set True
+    '''
+    file_size = (0.00775 * number_files) - 0.00578
+
+    if image_save:
+        file_size = (0.00794 * number_files) - 0.00578
+
+    file_size = round(file_size, 2)
+    return file_size
+
+
+def normalise_process_time(number_files,
+                           image_save=False):
+    '''
+    Calculates the total processing time for normalising images
+    and processing a data cube.
+    Args:
+        number_files: <int> number of files to process
+        image_save: <bool> if images are saved set True
+    '''
+    process_time = -0.3+(2.5*np.exp((number_files-173.2)/324.3))
+
+    if image_save:
+        process_time = -3.7+(2.9*np.exp((number_files+290.5)/421.9))
+
+    process_time = round(process_time, 1)
+    return process_time
+
+def processing_parameters(main_dir,
+                          exp_settings,
+                          image_save=False):
+    '''
+    Calculate the total processing time based on the number of files
+    present in each hs_img. Give the option to output a time for
+    both image output and data output.
+    '''
+    no_files = []
+    for hs_img in exp_settings['hs_imgs']:
+        img_dir = os.path.join(main_dir, hs_img)
+        if not os.path.isdir(img_dir):
+            continue
+
+        data_files = io.extract_files(dir_name=img_dir,
+                                      file_string='img_')
+        no_files.append(len(data_files))
+
+    number_files = sum(no_files)
+    process_time = normalise_process_time(number_files,
+                                          image_save)
+    file_size = normalise_file_size(number_files,
+                                    image_save)
+
+    print(f'\nTotal Number Of Files: {number_files}')
+    print(f'Image Save Set To: {image_save}')
+    print(f'Total File Size Approximately: {file_size} GB')
+    print(f'\nImage Save Set To: {image_save}')
+    print(f'Total Processing Time Approximately: {process_time} mins')
+
+
+
+if __name__ == '__main__':
+
+    number_files = [100, 200, 300, 400, 500, 600, 700, 800]
+    image_save = [True, False]
+    for files in number_files:
+        for save in image_save:
+            file_size = normalise_file_size(files, image_save=save)
+            process_time = normalise_process_time(files, image_save=save)
